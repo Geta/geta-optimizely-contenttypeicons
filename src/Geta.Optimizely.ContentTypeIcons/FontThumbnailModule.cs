961c7b6bb590114aa0772ac04eacb6825be18c83
Māris Krivtežs <marisks@gmail.com>
2021-04-29 15:06:34 +0300

    Formatting.

--- src/Geta.Optimizely.ContentTypeIcons/FontThumbnailModule.cs
+++ src/Geta.Optimizely.ContentTypeIcons/FontThumbnailModule.cs
@@ -1,14 +1,18 @@
 ﻿using EPiServer.Framework.TypeScanner;
+using EPiServer.Shell.Modules;
+using Microsoft.Extensions.FileProviders;
 
 namespace Geta.Optimizely.ContentTypeIcons
 {
     public class FontThumbnailModule : ShellModule
     {
-        public FontThumbnailModule(string name, string routeBasePath, string resourceBasePath) : base(name, routeBasePath, resourceBasePath)
+        public FontThumbnailModule(string name, string routeBasePath, string resourceBasePath)
+            : base(name, routeBasePath, resourceBasePath)
         {
         }
 
-        public FontThumbnailModule(string name, string routeBasePath, string resourceBasePath, ITypeScannerLookup typeScannerLookup, VirtualPathProvider virtualPathProvider) : base(name, routeBasePath, resourceBasePath, typeScannerLookup, virtualPathProvider)
+        public FontThumbnailModule(string name, string routeBasePath, string resourceBasePath, ITypeScannerLookup typeScannerLookup, IFileProvider virtualPathProvider)
+            : base(name, routeBasePath, resourceBasePath, typeScannerLookup, virtualPathProvider)
         {
         }
     }
