6f9feb65667007f42dc04cbca0ae1893899d3212
Māris Krivtežs <marisks@gmail.com>
2021-05-03 17:27:43 +0300

    Added sample usage.

--- sandbox/AlloyMvcTemplates/Models/Pages/StandardPage.cs
+++ sandbox/AlloyMvcTemplates/Models/Pages/StandardPage.cs
@@ -2,14 +2,17 @@
 using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
 using System.ComponentModel.DataAnnotations;
+ using Geta.Optimizely.ContentTypeIcons;
+ using Geta.Optimizely.ContentTypeIcons.Attributes;
 
-namespace AlloyMvcTemplates.Models.Pages
+ namespace AlloyMvcTemplates.Models.Pages
 {
     /// <summary>
     /// Used for the pages mainly consisting of manually created content such as text, images, and blocks
     /// </summary>
     [SiteContentType(GUID = "9CCC8A41-5C8C-4BE0-8E73-520FF3DE8267")]
-    [SiteImageUrl(Global.StaticGraphicsFolderPath + "page-type-thumbnail-standard.png")]
+    [TreeIcon(FontAwesome.AddressBook)]
+    [ContentTypeIcon(FontAwesome.AlignCenter)]
     public class StandardPage : SitePageData
     {
         [Display(
