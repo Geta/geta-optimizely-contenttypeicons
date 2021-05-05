8f518fee3cc572613809d199320a29813d3916c3
Māris Krivtežs <marisks@gmail.com>
2021-05-05 14:59:29 +0300

    Fixed file streaming.

--- src/Geta.Optimizely.ContentTypeIcons/Controllers/ContentTypeIconController.cs
+++ src/Geta.Optimizely.ContentTypeIcons/Controllers/ContentTypeIconController.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Text.RegularExpressions;
-using Geta.Optimizely.ContentTypeIcons.Infrastructure.Mvc;
 using Geta.Optimizely.ContentTypeIcons.Settings;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -28,13 +28,15 @@ namespace Geta.Optimizely.ContentTypeIcons.Controllers
 
             var image = _contentTypeIconService.LoadIconImage(settings);
 
-            return new ImageResult { Image = image, ImageFormat = ImageFormat.Png };
+            var stream = new MemoryStream();
+            image.Save(stream, ImageFormat.Png);
+            stream.Seek(0, SeekOrigin.Begin);
+            return new FileStreamResult(stream, "image/png");
         }
 
         internal bool CheckValidFormatHtmlColor(string inputColor)
         {
-            if (Regex.Match(inputColor, "^#(?:[0-9a-fA-F]{3}){1,2}$").Success)
-                return true;
+            if (Regex.Match(inputColor, "^#(?:[0-9a-fA-F]{3}){1,2}$").Success) return true;
 
             var result = System.Drawing.Color.FromName(inputColor);
             return result.IsKnownColor;
