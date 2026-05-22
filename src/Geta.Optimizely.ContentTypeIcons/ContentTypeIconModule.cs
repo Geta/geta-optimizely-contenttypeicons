e24b05050dc429acabfcd76df20b62b772b1eb1b
ivanmarkovic1402 <ivan.markovic1402@gmail.com>
2026-05-22 14:32:09 +0200

    foundation fixed in sub. content type icons work

--- src/Geta.Optimizely.ContentTypeIcons/ContentTypeIconModule.cs
+++
@@ -1,6 +0,0 @@
-namespace Geta.Optimizely.ContentTypeIcons
-{
-    public class ContentTypeIconModule
-    {
-    }
-}
--- tests/Geta.Optimizely.ContentTypeIcons.Tests/TestPhysicalPathResolver.cs
+++
@@ -1,5 +0,0 @@
-namespace Geta.Optimizely.ContentTypeIcons.Tests
-{
-    // Removed: IPhysicalPathResolver was made internal in CMS 13.
-    // Path resolution is now handled by ContentTypeIconService using IWebHostEnvironment.
-}
